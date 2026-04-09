3acd2cc4b168a46d5a962c5fc1ab0df7c62ebe87
박진현 <jbag4114@gmail.com>
2026-04-09 22:29:23 +0900

    feat: Employer와 JobPosting 양방향 연관관계 매핑"

--- backend/src/main/java/kr/ac/dankook/ace/smart_recruit/model/employer/Employer.java
+++ backend/src/main/java/kr/ac/dankook/ace/smart_recruit/model/employer/Employer.java
@@ -1,6 +1,9 @@
 package kr.ac.dankook.ace.smart_recruit.model.employer;
 
+import java.util.*;
+
 import jakarta.persistence.*;
+import kr.ac.dankook.ace.smart_recruit.model.jobposting.JobPosting;
 import kr.ac.dankook.ace.smart_recruit.model.member.Member;
 import lombok.*;
 
@@ -16,6 +19,10 @@ public class Employer {
     @OneToOne(fetch = FetchType.LAZY) // 지연 방식(안전성)
     @JoinColumn(name = "member_id", nullable = false) // Member의 PK를 참조하여 member_id에 넣음, 데이터의 주인, 연관관계의 주인
     private Member member;
+
+    // DB column으로 생성되지 않는 자바 객체 내부에서 존재하는 가상의 관계
+    @OneToMany(mappedBy = "employer", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<JobPosting> jobPostings = new ArrayList<>();
 
     @Column(name = "company_name", length = 50)
     private String companyName;
